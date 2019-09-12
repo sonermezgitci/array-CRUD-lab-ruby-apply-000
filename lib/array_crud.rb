11ea85f255719b8be24f5665dffcc2ea13e1fecc
Soner Mezgitci <mezgitci9@gmail.com>
2019-09-12 01:06:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,33 +1,33 @@
 def create_an_empty_array
-  
+[]    
 end
 
 def create_an_array
-  
+  ["soner","table","computer","john"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+                    array << element 
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def  add_element_to_start_of_array(array, element)
+  #unshift         array << element
 end
 
 def remove_element_from_end_of_array(array)
-  
+  #pop             array <<
 end
 
 def remove_element_from_start_of_array(array)
-  
+  #shift          array <<
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+                   [array <<"index_number"] 
 end
 
 def retrieve_first_element_from_array(array)
-  
+                    ["0,0"]
 end
 
 def retrieve_last_element_from_array(array)
